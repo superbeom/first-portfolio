98db8436f1269358ffd278e2a2f74e688a84b88b
superbeom <46369349+superbeom@users.noreply.github.com>
2022-11-19 10:17:43 +0900

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="This is description" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <header></header>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Home;
