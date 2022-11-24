37884c83bf395dbbe512b288f2fd813784e707f4
superbeom <it.super.beom@gmail.com>
2022-11-24 10:50:03 +0900

    Update tiny..

--- components/About.tsx
+++ components/About.tsx
@@ -9,38 +9,36 @@ interface Props {
   user: User;
 }
 
-const About = ({ user }: Props) => {
-  return (
-    <MotionContainer
-      className="min-h-screen h-full motion-container lg:max-w-5xl xl:max-w-7xl text-center lg:text-left
+const About = ({ user }: Props) => (
+  <MotionContainer
+    className="min-h-screen h-full motion-container lg:max-w-5xl xl:max-w-7xl text-center lg:text-left
     justify-start sm:justify-evenly"
-    >
-      <h2 className="title">About</h2>
+  >
+    <h2 className="title">About</h2>
 
-      <motion.img
-        initial={{ x: -200, opacity: 0 }}
-        transition={{ duration: 1.2 }}
-        whileInView={{ x: 0, opacity: 1 }}
-        className="w-44 h-44 flex-shrink-0 mt-32 ball object-cover md:rounded-lg md:w-[300px] md:h-[400px] xl:w-[500px] xl:h-[600px]"
-        src={user.image}
-      />
+    <motion.img
+      initial={{ x: -200, opacity: 0 }}
+      transition={{ duration: 1.2 }}
+      whileInView={{ x: 0, opacity: 1 }}
+      className="w-44 h-44 flex-shrink-0 mt-32 ball object-cover md:rounded-lg md:w-[300px] md:h-[400px] xl:w-[500px] xl:h-[600px]"
+      src={user.image}
+    />
 
-      <div className="space-y-10 px-0 mt-8 sm:mt-0 sm:px-10">
-        <div className="space-y-5">
-          <h3 className="text-3xl sm:text-4xl font-semibold tracking-wider">
-            Hey, I'm{" "}
-            <span className="underline decoration-primary underline-offset-8">
-              {user.name}
-            </span>
-          </h3>
+    <div className="space-y-10 px-0 mt-8 sm:mt-0 sm:px-10">
+      <div className="space-y-5">
+        <h3 className="text-3xl sm:text-4xl font-semibold tracking-wider">
+          Hey, I'm{" "}
+          <span className="underline decoration-primary underline-offset-8">
+            {user.name}
+          </span>
+        </h3>
 
-          <h4 className="about-role">{user.role}</h4>
-        </div>
-
-        <p>{user.background}</p>
+        <h4 className="about-role">{user.role}</h4>
       </div>
-    </MotionContainer>
-  );
-};
+
+      <p>{user.background}</p>
+    </div>
+  </MotionContainer>
+);
 
 export default About;
--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -8,44 +8,42 @@ interface Props {
   user: User;
 }
 
-const ContactMe = ({ user }: Props) => {
-  return (
-    <div className="relative layout flex-center w-screen px-5 md:px-10">
-      <h2 className="title">Contact</h2>
+const ContactMe = ({ user }: Props) => (
+  <div className="relative layout flex-center w-screen px-5 md:px-10">
+    <h2 className="title">Contact</h2>
 
-      <div className="flex flex-col space-y-20 md:space-y-32 w-full">
-        <h3 className="text-2xl md:text-4xl font-medium md:font-semibold text-center">
-          Just{" "}
-          <span className="underline decoration-primary underline-offset-8">
-            feel free to
-          </span>{" "}
-          talk to me!
-        </h3>
+    <div className="flex flex-col space-y-20 md:space-y-32 w-full">
+      <h3 className="text-2xl md:text-4xl font-medium md:font-semibold text-center">
+        Just{" "}
+        <span className="underline decoration-primary underline-offset-8">
+          feel free to
+        </span>{" "}
+        talk to me!
+      </h3>
 
-        <div className="space-y-10 md:space-y-20">
-          <div className="contact-detail">
-            <FaPhoneAlt className="contact-icon" />
-            <p>{user.phone}</p>
-          </div>
+      <div className="space-y-10 md:space-y-20">
+        <div className="contact-detail">
+          <FaPhoneAlt className="contact-icon" />
+          <p>{user.phone}</p>
+        </div>
 
-          <div className="contact-detail">
-            <FaEnvelope className="contact-icon" />
-            <p>{user.email}</p>
-          </div>
+        <div className="contact-detail">
+          <FaEnvelope className="contact-icon" />
+          <p>{user.email}</p>
+        </div>
 
-          <div className="contact-detail">
-            <FaMapMarkerAlt className="contact-icon" />
-            <p>{user.address}</p>
-          </div>
+        <div className="contact-detail">
+          <FaMapMarkerAlt className="contact-icon" />
+          <p>{user.address}</p>
+        </div>
 
-          <div className="contact-detail">
-            <AiFillMessage className="contact-icon" />
-            <p>Click ME on the bottom right!</p>
-          </div>
+        <div className="contact-detail">
+          <AiFillMessage className="contact-icon" />
+          <p>Click ME on the bottom right!</p>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default ContactMe;
