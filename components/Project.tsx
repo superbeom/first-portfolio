8b561b0629b5d42d8d9c757aa9cb3a2d60356531
superbeom <it.super.beom@gmail.com>
2022-11-25 15:15:18 +0900

    Update Project part 2

--- components/Project.tsx
+++ components/Project.tsx
@@ -8,11 +8,41 @@ interface Props {
   project: Project;
 }
 
-const position = 500;
+interface DetailsProps {
+  position: number;
+  divStyle: string;
+  h3Style: string;
+  pStyle: string;
+  projectName: string;
+  projectSummary: string;
+}
+
 const transition = { duration: 1 };
 const whileInView = { y: 0, opacity: 1 };
 
 const width = "w-[100%] 2xl:w-[80%]";
+
+const Details = ({
+  position,
+  divStyle,
+  h3Style,
+  pStyle,
+  projectName,
+  projectSummary,
+}: DetailsProps) => (
+  <motion.div
+    initial={{ y: position, opacity: 0 }}
+    transition={transition}
+    whileInView={whileInView}
+    className={divStyle}
+  >
+    <h3 className={`${h3Style} font-semibold`}>
+      <span className="underline underline-offset-4">{projectName}</span>
+    </h3>
+
+    <p className={pStyle}>{projectSummary}</p>
+  </motion.div>
+);
 
 const Project = ({ project }: Props) => {
   const { isMobile, isDesktop } = useStore();
@@ -26,34 +56,24 @@ const Project = ({ project }: Props) => {
       />
 
       {isDesktop ? (
-        <motion.div
-          initial={{ y: position, opacity: 0 }}
-          transition={transition}
-          whileInView={whileInView}
-          className={`absolute top-0 flex-center flex-col ${width} h-full
-        space-y-5 bg-dark-background-50 text-white-light`}
-        >
-          <h3 className="text-4xl font-semibold">
-            <span className="underline underline-offset-4">{project.name}</span>
-          </h3>
-
-          <p className="text-2xl">{project.summary}</p>
-        </motion.div>
+        <Details
+          position={500}
+          divStyle={`absolute top-0 flex-center flex-col ${width} h-full
+          space-y-5 bg-dark-background-50 text-white-light`}
+          h3Style="text-4xl"
+          pStyle="text-2xl"
+          projectName={project.name}
+          projectSummary={project.summary}
+        />
       ) : (
-        <motion.div
-          initial={{ y: -50, opacity: 0 }}
-          transition={{ duration: 1 }}
-          whileInView={{ y: 0, opacity: 1 }}
-          className={`flex-center flex-col ${width} h-full space-y-3 mt-3 text-center`}
-        >
-          <h3 className={`${isMobile ? "text-2xl" : "text-3xl"} font-semibold`}>
-            <span className="underline underline-offset-4">{project.name}</span>
-          </h3>
-
-          <p className={`${isMobile ? "text-lg" : "text-xl"}`}>
-            {project.summary}
-          </p>
-        </motion.div>
+        <Details
+          position={-50}
+          divStyle={`flex-center flex-col ${width} h-full space-y-3 mt-3 text-center`}
+          h3Style={`${isMobile ? "text-2xl" : "text-3xl"}`}
+          pStyle={`${isMobile ? "text-lg" : "text-xl"}`}
+          projectName={project.name}
+          projectSummary={project.summary}
+        />
       )}
     </div>
   );
